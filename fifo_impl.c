02d76a9cb7ce7843d4fc0d204d6860e0337d6c88
kent80830 <3135552898@qq.com>
2026-05-19 13:31:15 +0800

    Update fifo_impl.c

--- fifo_impl.c
+++ fifo_impl.c
@@ -1,51 +1,87 @@
-#ifndef IPC_COMMON_H
-#define IPC_COMMON_H
+#include "ipc_common.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/msg.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <time.h>
+typedef struct {
+    int fd_read;
+    int fd_write;
+} fifo_handle_t;
 
-#define LOG_INFO(fmt, ...) printf("[INFO] " fmt "\n", ##__VA_ARGS__)
-#define LOG_ERROR(fmt, ...) fprintf(stderr, "[ERROR] " fmt "\n", ##__VA_ARGS__)
+fifo_handle_t* fifo_server_init(void) {
+    fifo_handle_t *h = malloc(sizeof(fifo_handle_t));
+    if (!h) return NULL;
+    
+    unlink(FIFO_PATH_RD);
+    unlink(FIFO_PATH_WR);
+    
+    mkfifo(FIFO_PATH_RD, 0666);
+    mkfifo(FIFO_PATH_WR, 0666);
+    
+    h->fd_read = open(FIFO_PATH_RD, O_RDWR);
+    h->fd_write = open(FIFO_PATH_WR, O_RDWR);
+    
+    if (h->fd_read == -1 || h->fd_write == -1) {
+        free(h);
+        return NULL;
+    }
+    
+    LOG_INFO("FIFO Server: ready");
+    return h;
+}
 
-#define FIFO_PATH_RD "./fifo_rd"
-#define FIFO_PATH_WR "./fifo_wr"
-#define MSGQ_KEY 0x1234
+fifo_handle_t* fifo_client_init(void) {
+    fifo_handle_t *h = malloc(sizeof(fifo_handle_t));
+    if (!h) return NULL;
+    
+    h->fd_write = open(FIFO_PATH_RD, O_RDWR);
+    h->fd_read = open(FIFO_PATH_WR, O_RDWR);
+    
+    if (h->fd_write == -1 || h->fd_read == -1) {
+        free(h);
+        return NULL;
+    }
+    
+    LOG_INFO("FIFO Client: connected");
+    return h;
+}
 
-typedef enum {
-    IPC_TYPE_FIFO = 0,
-    IPC_TYPE_MSGQ
-} ipc_type_t;
+int fifo_send(fifo_handle_t *h, const void *data, size_t len) {
+    ssize_t ret;
+    size_t total = 0;
+    const char *ptr = (const char*)data;
+    
+    while (total < len) {
+        ret = write(h->fd_write, ptr + total, len - total);
+        if (ret == -1) {
+            if (errno == EINTR) continue;
+            return -1;
+        }
+        total += ret;
+    }
+    return 0;
+}
 
-typedef struct perf_data {
-    double avg_latency_us;
-    double throughput_mbps;
-    int total_bytes;
-    double total_time_us;
-    int success_count;
-} perf_data_t;
+int fifo_recv(fifo_handle_t *h, void *buf, size_t len) {
+    ssize_t ret;
+    size_t total = 0;
+    char *ptr = (char*)buf;
+    
+    while (total < len) {
+        ret = read(h->fd_read, ptr + total, len - total);
+        if (ret == -1) {
+            if (errno == EINTR) continue;
+            return -1;
+        }
+        if (ret == 0) return -1;
+        total += ret;
+    }
+    return 0;
+}
 
-typedef struct ipc_handle {
-    ipc_type_t type;
-    void *impl;
-    int (*init_server)(struct ipc_handle *handle);
-    int (*init_client)(struct ipc_handle *handle);
-    int (*send)(struct ipc_handle *handle, const void *data, size_t len);
-    int (*recv)(struct ipc_handle *handle, void *buf, size_t len);
-    int (*close)(struct ipc_handle *handle);
-} ipc_handle_t;
-
-ipc_handle_t* ipc_create(ipc_type_t type);
-void ipc_destroy(ipc_handle_t *handle);
-
-#endif
+void fifo_close(fifo_handle_t *h) {
+    if (h) {
+        if (h->fd_read > 0) close(h->fd_read);
+        if (h->fd_write > 0) close(h->fd_write);
+        unlink(FIFO_PATH_RD);
+        unlink(FIFO_PATH_WR);
+        free(h);
+    }
+}
