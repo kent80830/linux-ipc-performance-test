c703302fb66f7a9a1ba3cfe23f37b20f58382d13
kent80830 <3135552898@qq.com>
2026-05-19 13:30:47 +0800

    Update ipc_common.h

--- ipc_common.h
+++ ipc_common.h
@@ -22,10 +22,10 @@
 #define FIFO_PATH_WR "./fifo_wr"
 #define MSGQ_KEY 0x1234
 
-typedef enum {
-    IPC_TYPE_FIFO = 0,
-    IPC_TYPE_MSGQ
-} ipc_type_t;
+#define IPC_TYPE_FIFO 0
+#define IPC_TYPE_MSGQ 1
+
+#define CURRENT_IPC_TYPE IPC_TYPE_FIFO
 
 typedef struct {
     double avg_latency_us;
@@ -34,5 +34,16 @@ typedef struct {
     double total_time_us;
     int success_count;
 } perf_data_t;
+
+typedef struct {
+    int type;
+    void *handle;
+} ipc_handle_t;
+
+ipc_handle_t* ipc_server_init(int type);
+ipc_handle_t* ipc_client_init(int type);
+int ipc_send(ipc_handle_t *h, const void *data, size_t len);
+int ipc_recv(ipc_handle_t *h, void *buf, size_t len);
+void ipc_close(ipc_handle_t *h, int is_server);
 
 #endif
